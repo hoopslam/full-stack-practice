cfc3c7dc05af323f4174c2a1f6177cc06450de71
David Cho <david@Davids-MacBook-Pro.local>
2022-09-29 15:29:24 +0900

    feat: create project base files

---
+++ server/data.js
@@ -0,0 +1,72 @@
+const projects = [
+  {
+    id: `1`,
+    clientId: `1`,
+    name: `eCommerce Website`,
+    description: `Lorem`,
+    status: `In Progress`,
+  },
+  {
+    id: `2`,
+    clientId: `2`,
+    name: `eCommerce Website`,
+    description: `Lorem`,
+    status: `In Progress`,
+  },
+  {
+    id: `3`,
+    clientId: `3`,
+    name: `eCommerce Website`,
+    description: `Lorem`,
+    status: `In Progress`,
+  },
+  {
+    id: `4`,
+    clientId: `4`,
+    name: `eCommerce Website`,
+    description: `Lorem`,
+    status: `In Progress`,
+  },
+  {
+    id: `5`,
+    clientId: `5`,
+    name: `eCommerce Website`,
+    description: `Lorem`,
+    status: `In Progress`,
+  },
+];
+
+const clients = [
+  {
+    id: `1`,
+    name: `Mya Buttreeks`,
+    email: `stinky@gmail.com`,
+    phone: `523-456-7890`,
+  },
+  {
+    id: `2`,
+    name: `Amanda Huggenkiss`,
+    email: `lonely@gmail.com`,
+    phone: `323-456-7890`,
+  },
+  {
+    id: `3`,
+    name: `I.P. Freely`,
+    email: `free@gmail.com`,
+    phone: `623-456-7890`,
+  },
+  {
+    id: `4`,
+    name: `Hugh Jass`,
+    email: `jass@gmail.com`,
+    phone: `723-456-7890`,
+  },
+  {
+    id: `5`,
+    name: `Ahmed Adoudi`,
+    email: `messy@gmail.com`,
+    phone: `923-456-7890`,
+  },
+];
+
+module.exports = { projects, clients };
